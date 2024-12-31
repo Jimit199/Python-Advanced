8ab59fdc4da6fbd695d5730dafc6c3b4feae98f9
Jimit199 <shahsd185@gmail.com>
2024-12-30 20:20:34 -0500

    Add files via upload

---
+++ Introduction/main.py
@@ -0,0 +1,19 @@
+name="jimit"
+num=13
+print("My name is", name,"I am", num,"years old.")
+print(13)
+
+#This is for the place
+
+place=input("Type your school name:")
+print("I go to",place)
+
+#This is for the favortite game of someone.
+
+game=input("What is your favorite game or video game:")
+print("Your favorite game is",game)
+
+#Inport Keyword. This is something else meant for the Library.
+
+import keyword
+print(keyword.kwlist)
